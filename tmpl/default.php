4fa5388745aa9919a302b06fc22ddcad2e81f41a
Eric Korolev <3254@mail.ru>
2017-12-08 10:58:04 +0500

    Change open/close button by scrolling

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,35 +1,41 @@
 <?php
 /**
  * Шаблон модуля "Топ по сумме пожертвований".
+ * Шаблон отображает таблицу в области не более 220px.
+ * Содержимое таблицы превышает заданный размер области,
+ * поэтому задается скроллинг для прокрутки области.
  */
  
  defined('_JEXEC') or die; ?>
  
  <?php
-// Подключение javascript файлов
-$document = JFactory::getDocument();
-$document->addScript('/modules/mod_top/js/hide.js');
+ // Подключение css стилей модуля
+ $document = JFactory::getDocument();
+ // Подключение стиля для скроллирования области
+ $document->addStyleSheet('/modules/mod_top/css/scroll.css');
 ?>
- 
-<table id="donat">
-<tr>
-<td style="width: 33px;" align="center"><strong>№</strong></td>
-<td style="width: 148px;"><strong>Ник</strong></td>
-<td style="width: 107px;"><strong>Рубли</strong></td>
-</tr>
-<?php
 
-$position = 1;	
+<h3>Топ по по сумме пожертвований</h3>
 
-	foreach ($top as $row) {
-		echo '<tr">';
-		echo '<td>' . $position . '</td>';
-        echo '<td>' . $row['0'] . '</td>';
-		echo '<td>' . $row['1'] . '</td>';
-		echo '</tr>';
-		$position ++;
-	}
-?>
+<div class="scroll">
+     <table id="donat">
+     <tr>
+     <td style="width: 33px;" align="center"><strong>№</strong></td>
+     <td style="width: 148px;"><strong>Ник</strong></td>
+     <td style="width: 107px;"><strong>Рубли</strong></td>
+     </tr>
+     <?php
+
+     $position = 1;
+
+     	foreach ($top as $row) {
+     		echo '<tr">';
+     		echo '<td>' . $position . '</td>';
+             echo '<td>' . $row['0'] . '</td>';
+     		echo '<td>' . $row['1'] . '</td>';
+     		echo '</tr>';
+     		$position ++;
+     	}
+     ?>
 </table>
-<input id="show" type="button" value="Раскрыть">
-<input id="hide" type="button" value="Скрыть">
+</div>
